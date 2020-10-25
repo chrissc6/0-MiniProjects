b1bb3702aab89c04b53a444ce9f26a9e992a77fe
chris <christopherscaudill@gmail.com>
2020-10-25 19:49:41 -0400

    Data Models

---
+++ CoolUniversityMVCWebApp/CoolUniversity/CoolUniversity/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolUniversity.Models
+{
+    public class Course
+    {
+        //attribute lets you enter the primary key for the course rather than having the database generate it
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ CoolUniversityMVCWebApp/CoolUniversity/CoolUniversity/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolUniversity.Models
+{
+    public class Student
+    {
+        //primary key
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //navigation property
+        //a Student entity will hold all of the Enrollment entities that are related to that Student entity
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
